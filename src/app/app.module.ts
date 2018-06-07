c84c6775da8525e92dcda61469c56327390b03c7
HaiveroV <mitkohaiverov@abv.bg>
2018-06-07 23:39:02 +0300

    Fixed some components in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './components/homepage/homepage.component';
+import {AboutComponent} from './components/about/about.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {SearchComponent} from './components/search/search.component';
+import {TimelineComponent} from './components/timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    NotFoundComponent,
+    FooterComponent,
+    NavbarComponent,
+    SearchComponent,
+    TimelineComponent
+
   ],
   imports: [
     BrowserModule
@@ -15,4 +29,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

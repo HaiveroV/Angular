6e54ee0e926a8fbbffcec0c1ea85f8e96088da3c
HaiveroV <mitkohaiverov@abv.bg>
2018-06-12 19:46:05 +0300

    Added production keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD_DIFD5qiR13egPW9BQzxKECIl7km7Pxo",
+    authDomain: "historyapp-8df4d.firebaseapp.com",
+    databaseURL: "https://historyapp-8df4d.firebaseio.com",
+    projectId: "historyapp-8df4d",
+    storageBucket: "historyapp-8df4d.appspot.com",
+    messagingSenderId: "747121332994"
+  }
 };
